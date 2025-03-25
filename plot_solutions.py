9e66122cd41c9259c7d5f60fba6ff5bcf3008b72
beckertje <76971095+beckertje@users.noreply.github.com>
2025-03-25 15:21:31 +0100

    Create plot_solutions.py
    python code for plotting "all_final" array from main code

---
+++ plot_solutions.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt(r"C:\Users\tombe\Documents\Master\Scientific Computing\Project\EmptyVSCodeProject\build\src\solutions.txt")
+
+plt.figure(figsize=(10, 6))
+for i in range(2, min(7, data.shape[0])): 
+    plt.plot(data[i], label=f'State {i-2}')
+
+plt.xlabel("x (index number)")
+plt.ylabel("y")
+plt.title("First 5 solutions")
+plt.legend()
+plt.grid()
+plt.show()
